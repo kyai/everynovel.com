2b9786d3a74295fbacce1c3050b932dd1d20ea88
kyai <hukuang@163.com>
2017-04-23 21:22:42 +0800

    submit

--- src/everynovel/controllers/word.go
+++ src/everynovel/controllers/word.go
@@ -1,13 +1,73 @@
 package controllers
 
 import (
-	"github.com/astaxie/beego"
+    "fmt"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+    "time"
 )
 
 type WordController struct {
-	beego.Controller
+    BaseController
 }
 
-func (c *WordController) Get() {
-	c.TplName = "index.tpl"
+type Word struct {
+    Id_ 		bson.ObjectId `bson:"_id" json:"id"`
+    Title 		string `json:"title"`
+    Content     string `json:"content"`
+    Date        string `json:"date"`
+    User 		string `json:"user"`
+}
+
+const (
+    URL = "47.90.49.118:28080"
+)
+
+func (c *WordController) GetWord() {
+    session, err := mgo.Dial(URL)  //连接数据库
+    if err != nil {
+        panic(err)
+    }
+    defer session.Close()
+    session.SetMode(mgo.Monotonic, true)
+
+    collection := session.DB("everynovel").C("en_word")
+
+    infs := []Word{}
+    err = collection.Find(&bson.M{}).Sort("-date").All(&infs)
+
+    Id, _ := c.GetInt32("id")
+    c.EchoJSON("111", string(Id), infs)
+}
+
+func (c *WordController) PostWord() {
+    session, err := mgo.Dial(URL)  //连接数据库
+    if err != nil {
+        panic(err)
+    }
+    defer session.Close()
+    session.SetMode(mgo.Monotonic, true)
+
+    collection := session.DB("everynovel").C("en_word")
+
+    title := c.GetString("title")
+    content := c.GetString("content")
+
+    fmt.Println("===")
+    fmt.Println(title)
+    fmt.Println(content)
+
+	word := &Word{
+		Id_: bson.NewObjectId(),
+		Title: title,
+		Content: content,
+		Date: time.Now().Format("2006-01-02 15:04:05"),
+	}
+
+	err = collection.Insert(word)
+	if err != nil {
+		panic(err)
+	}
+
+    c.EchoJSON("111", "success", "")
 }
--- src/everynovel/routers/router.go
+++ src/everynovel/routers/router.go
@@ -6,6 +6,9 @@ import (
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+    // beego.Router("/", &controllers.MainController{})
+    beego.Router("/", &controllers.IndexController{})
     beego.Router("/index", &controllers.IndexController{})
+    beego.Router("/GetWord", &controllers.WordController{}, "get:GetWord")
+    beego.Router("/PostWord", &controllers.WordController{}, "post:PostWord")
 }
